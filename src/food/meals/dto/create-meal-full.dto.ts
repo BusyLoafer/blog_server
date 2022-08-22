1903b2d428195a55f699edf25e09e3ce676ee195
Oshchepkov <oshchepkov@iMac-Osepkov.local>
2022-08-22 16:46:26 +0300

    fix

--- src/food/meals/dto/create-meal-full.dto.ts
+++ src/food/meals/dto/create-meal-full.dto.ts
@@ -1,14 +1,25 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { Dish } from "src/food/dishes/dishes.entity";
+import { Product } from "src/food/products/products.entity";
 
 export class CreateMealFullDto {
 
-  @ApiProperty({ example: 1, description: 'Тип приема' })
-  readonly meal_type_id: number;
+  @ApiProperty({ example: {
+    "id": 1,
+    "title": "Ужин",
+    "description": "плотный приём пищи после 16:00 "
+  }, description: 'Тип приема' })
+  readonly MealType: number;
 
   @ApiProperty({ example: '12/12/12 12:00:00', description: 'Дата приема пищи' })
   readonly date: string;
 
-  @ApiProperty({ example: [1, 2, 3], description: 'Идентификаторы блюд' })
-  readonly dishes: number[];
-
+  @ApiProperty({ example: [{
+    id: 1,
+    title: "Вареный рис с мясом",
+    description: "Вареный бурый рис и запеченное мясо",
+    resipe: "Сварить рис, запечь курицу"
+  }], description: 'Ингредиенты' })
+  readonly dishes: Dish[];
+  
 }
